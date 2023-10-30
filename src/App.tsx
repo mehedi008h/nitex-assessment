ab4f76ba9e4dd13411021a8c17165ed9ae43db48
Mehedi Hasan <mehedi08h@gmail.com>
2023-10-30 10:15:54 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Hero } from "./components";
+import "./App.scss";
+
+function App() {
+    return (
+        <>
+            <Hero />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,10 @@
+import styles from "./hero.module.scss";
+const Hero = () => {
+    return (
+        <main className={styles.hero}>
+            <h1 className="bold-text">Hero Section</h1>
+        </main>
+    );
+};
+
+export default Hero;
